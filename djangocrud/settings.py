ff589e6a2f718f7cc3c76fb062ad11ccce62349b
Noe Cabanillas <73083315+nCabanillas@users.noreply.github.com>
2023-04-22 22:53:24 -0500

    second commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tasks"
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+LOGIN_URL = "/signin"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
+from .models import Task 
 
 # Create your views here.
+def signup(request):
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
+    else:
+        
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objests.create_user(request.POST["username"],password=request.POST["password1"])
+            except IntegrityError:
+                return render(request, 'signup.html', {"form":UserCreationForm, "error":"Username already taken."})
+    
+    return render(request, 'signup.html', {"form": UserCreationForm, "error":"Passwords did not match."})
+
+@login_required
+def tasks(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'tasks.html')
